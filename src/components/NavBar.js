dd44891782f1a1c44d0b000f060ee4bd898ed004
vikdts <v.d.tsareva@gmail.com>
2024-10-03 20:13:28 +0300

    add an img element inside navbar brand component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
     <Navbar expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo} alt="logo" height={45}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
