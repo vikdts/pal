e121018dd57522c6835a6f38655b6425a32c8a06
vikdts <v.d.tsareva@gmail.com>
2024-10-25 16:26:36 +0300

    remove links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,8 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
